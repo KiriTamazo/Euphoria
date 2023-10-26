0c1e5b081282a1ac0803646f6a8d8de2d6c1269c
LW Htet Aung <kiritokirikaya80@gmail.com>
2023-10-26 21:55:33 +0630

    finished set up

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,15 +1,17 @@
 const Navbar = () => {
     return (
-        <nav className="container py-4 shadow-md flex">
+        <nav className="container py-4 shadow-md flex items-center justify-between">
             <div className="">Navbar</div>
-            <ul className="flex">
+            <ul className="flex gap-6">
                 <li>Home</li>
                 <li>Products</li>
                 <li>Blogs</li>
                 <li>About Us</li>
                 <li>Contact Us</li>
             </ul>
-            <div className=""></div>
+            <div className="">
+                
+            </div>
         </nav>
     )
 }
