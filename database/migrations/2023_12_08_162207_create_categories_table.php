d7a286b657570a802df085286d8acf68a41f5168
LW Htet Aung <kiritokirikaya80@gmail.com>
2023-12-14 22:57:00 +0630

    add factory of categories and user

--- database/migrations/2023_12_08_162207_create_categories_table.php
+++ database/migrations/2023_12_08_162207_create_categories_table.php
@@ -4,22 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
+
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->unique(['name']);
+            $table->string('name')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('categories');
