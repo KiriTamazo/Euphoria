55e0ec991669dc267ac697f2bd5b5743dd8efdec
LW Htet Aung <kiritokirikaya80@gmail.com>
2023-12-17 00:52:18 +0630

    setup delete modal component

---
+++ resources/js/Components/DeleteModal.jsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button'
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
+import { X } from 'lucide-react'
+
+
+
+const DeleteModal = ({modal=false,setModal,handleDelete,btnText='Delete',title='Are you sure?',description='Do you really want to delete these records?.\n This process cannot be undone.'}) => {
+    return (
+        <Dialog open={modal} onOpenChange={(state)=>setModal(state)}>
+            <DialogTrigger asChild>
+                <Button variant="danger">{btnText}</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <X className='border-danger border-4 text-danger mx-auto my-4 rounded-full w-16 h-16  p-2' />
+                </DialogHeader>
+                <DialogHeader className='text-center'>
+                    <DialogTitle className="text-2xl text-color text-center">{title}</DialogTitle>
+                    <DialogDescription className='py-4 text-center'>
+                        {description}
+                    </DialogDescription>
+                </DialogHeader>
+               
+                <DialogFooter>
+                    <DialogClose className='flex-1' asChild>
+                        <Button type="button" variant="secondary">
+              Close
+                        </Button>
+                    </DialogClose>
+                    <Button className='flex-1' onClick={handleDelete} variant='danger'>Delete</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+export default DeleteModal
--- resources/js/Components/component/DashboardSidebar.jsx
+++ resources/js/Components/component/DashboardSidebar.jsx
@@ -4,16 +4,25 @@ import { sidebarLinks } from '@/helpers/links'
 import { ChevronLeft } from 'lucide-react'
 import { useState } from 'react'
 import { Button } from '../ui/button'
+import { router } from '@inertiajs/react'
+import DeleteModal from '../DeleteModal'
 
 const DashboardSidebar = ({menuActive,setMenuActive}) => {
     const [menuHover,setMenuHover] = useState(false)
+    const [modal,setModal] = useState(false)
+    const handleLogout = () =>{
+        router.post('logout')
+    }
+    const handleDelete = () =>{
+        console.log('here')
+    }
     return (
         <aside onMouseEnter={()=>{
             if(!menuActive){
                 setMenuHover(true)}}
         }
         onMouseLeave={()=>setMenuHover(false)}
-        className={`${menuActive || menuHover ? 'w-[250px]' :'w-[90px]'} cursor-pointer bg-slate-200 z-50 fixed left-0 top-0 h-screen transition-all duration-500 ease-in-out flex flex-col` }>
+        className={`${menuActive || menuHover ? 'w-[250px]' :'w-[90px]'} cursor-pointer bg-white z-50 fixed left-0 top-0 h-screen transition-all duration-500 ease-in-out flex flex-col` }>
             <div onClick={()=>setMenuActive(!menuActive)} className=" flex justify-between relative p-4">
                 <p>Logo</p>
                 <ChevronLeft className={`${menuActive ? '': 'rotate-180 opacity-0 invisible'} ${menuHover ? 'opacity-100 !visible':''} cursor-pointer absolute  transition duration-500 ease-in-out -right-3 top-5 bg-slate-400 w-7 h-7 rounded-full`} />
@@ -22,15 +31,16 @@ const DashboardSidebar = ({menuActive,setMenuActive}) => {
                 {sidebarLinks?.map((link,i)=>{
                     return   (
                         <li key={i}>
-                            <div className='flex my-2 py-2 px-2 mx-4 rounded bg-blue-300 items-center  '>
+                            <div className='flex my-2 py-3 px-2 mx-4 rounded bg-primary/20 text-primary  items-center  '>
                                 <span className={'inline-flex justify-center items-center ml-[.7rem]'}> {link?.icon}</span> 
-                                <p className={'ml-4  text-sm tracking-wide truncate'}>{link?.name}</p>
+                                <p className={'ml-4  text-base tracking-wide truncate'}>{link?.name}</p>
                             </div>
                         </li>)
 
                 })}
             </ul>
-            <Button className="w-[90%] mb-2 bg-danger !mx-auto">Logout</Button>
+            <DeleteModal handleDelete={handleDelete} modal={modal} setModal={setModal} />
+            <Button onClick={handleLogout} variant='danger' className="w-[90%] mb-2 !mx-auto">Logout</Button>
         </aside>
     )
 }
